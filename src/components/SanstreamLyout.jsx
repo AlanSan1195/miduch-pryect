6a198bc8c57455c698770bf6be6f2f132c717342
Alan San <alansandovalt1@gmail.com>
2025-03-17 22:55:10 -0600

    createContext, RecommendedChannels and LiveChannels

--- src/components/SanstreamLyout.jsx
+++ src/components/SanstreamLyout.jsx
@@ -1,21 +1,20 @@
-import { useState } from "react";
-import {RecommendedChannels} from "./RecommendedChannels";
-import {LivesChannels} from "./LivesChannels";
+import { createContext, useContext, useState } from "react";
+//aprendimos **createContext()**
+const initialContext = createContext(null)
+//Basicamente es un componente que comparte un estado inicial y que le dices que renderizara lo que yo ponga dentro de el {{children}}
 
+export function SanstreamLyout({children}) {
 
-export function SanstreamLyout() {
-  const [isVisible, setIsVisible] = useState(true); // Estado compartido
+  const [context, setContext] = useState(true); // Estado compartido
 
   return (
-    <div className="flex  ">
-      <div className=" fixed z-00  ">
-
-      <RecommendedChannels visible={isVisible} setVisible={setIsVisible} />
-      </div>
-      <div className=" ml-60">
-      <LivesChannels  visible={isVisible} />
-
-      </div>
-    </div>
+    <>
+     <initialContext.Provider value = {{context, setContext}}>
+      {children}
+     </initialContext.Provider>
+    </>
   );
+}
+export function useInitialContext(){
+  return useContext(initialContext)
 }
