96116b443d883908e09837753f063cf227d1c9c4
Alan San <alansandovalt1@gmail.com>
2025-04-07 20:47:21 -0600

    add: auth-astro to projec

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,11 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), auth()],
   output:'server',
   adapter: netlify(),
 });
